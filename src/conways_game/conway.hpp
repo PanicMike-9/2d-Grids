89d7ff99d1a1c5fbcebc169bb4fe6bb6490a5ab9
PanicMike-9 <omsaudager990@gmail.com>
2026-03-21 20:03:05 +0000

    add update, display functions, move member variables to private

--- src/conways_game/conway.hpp
+++ src/conways_game/conway.hpp
@@ -3,11 +3,16 @@
 class Conway
 {
     public:
+        // constructor
+        Conway(int rows, int cols);
+
+        void update();
+
+        void display() const;
+
+    private:
         int rows;
         int cols;
         std::vector<std::vector<int>> curr_grid; // vector grid
         std::vector<std::vector<int>> next_grid; // vector grid
-
-        // constructor
-        Conway(int rows, int cols);
 };
